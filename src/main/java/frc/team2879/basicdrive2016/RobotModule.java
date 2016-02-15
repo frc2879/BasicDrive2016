ada214985b81cb4144e2444c63ebb0b7fe598add
Paul Friederichsen <floogulinc@gmail.com>
2016-02-15 11:59:39 -0600

    Add drive mode improvements and fix reversed joystick
    Add drive mode chooser on the dashboard and add arcade 2 stick mode.
    Also add some config options.

--- src/main/java/frc/team2879/basicdrive2016/RobotModule.java
+++ src/main/java/frc/team2879/basicdrive2016/RobotModule.java
@@ -3,9 +3,9 @@ package frc.team2879.basicdrive2016;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import jaci.openrio.toast.core.thread.Heartbeat;
 import jaci.openrio.toast.lib.log.Logger;
 import jaci.openrio.toast.lib.module.IterativeModule;
 import jaci.openrio.toast.lib.module.ModuleConfig;
@@ -21,14 +21,26 @@ public class RobotModule extends IterativeModule {
     public static boolean DRIVE_SQUAREDINPUTS;
     public static boolean DRIVE_BRAKE;
     
+    public static Integer[] TALONS_LEFT;
+    public static Integer[] TALONS_RIGHT;
+    
+    public static boolean TALONS_REVERSE_LEFT;
+    public static boolean TALONS_REVERSE_RIGHT;
+    
+    public static final String driveTank = "TANK";
+    public static final String driveArcade = "ARCADE";
+    public static final String driveArcade2Stick = "ARCADE2STICK";
+    
+    private SendableChooser driveChooser;
+    
     public RobotDrive robotDrive;
     
     public final XboxController driveJoystick = new XboxController(0);
     
-    public CANTalon leftTalon = Registrar.canTalon(1);
-    public CANTalon leftTalonF = Registrar.canTalon(2);
-    public CANTalon rightTalon = Registrar.canTalon(3);
-    public CANTalon rightTalonF = Registrar.canTalon(4);
+    public CANTalon leftTalon;
+    public CANTalon leftTalonF;
+    public CANTalon rightTalon;
+    public CANTalon rightTalonF;
 
     @Override
     public String getModuleName() {
@@ -37,7 +49,7 @@ public class RobotModule extends IterativeModule {
 
     @Override
     public String getModuleVersion() {
-        return "0.0.4";
+        return "0.0.5";
     }
     
     @Override
@@ -46,21 +58,50 @@ public class RobotModule extends IterativeModule {
         //TODO: Module Init
         
         pref = new ModuleConfig("BasicDrive2016");
-        DRIVE_TYPE = pref.getString("drive.type", "TANK");
+        DRIVE_TYPE = pref.getString("drive.type", driveTank);
         DRIVE_SQUAREDINPUTS = pref.getBoolean("drive.squaredinputs", false);
         DRIVE_BRAKE = pref.getBoolean("drive.brake", false);
+        
+        TALONS_LEFT = (Integer[]) pref.getArray("talons.left", new Integer[] {1, 2,});
+        TALONS_RIGHT = (Integer[]) pref.getArray("talons.right", new Integer[] {3, 4});
+        
+        TALONS_REVERSE_LEFT = pref.getBoolean("talons.reverse.left", false);
+        TALONS_REVERSE_RIGHT = pref.getBoolean("talons.reverse.right", false);
+        
+        driveChooser = new SendableChooser();
+        driveChooser.addDefault("Tank", driveTank);
+        driveChooser.addObject("Arcade", driveArcade);
+        driveChooser.addObject("Arcade 2 Stick", driveArcade2Stick);
+        SmartDashboard.putData("Drive Chooser", driveChooser);
         
         SmartDashboard.putString("Drive Type", DRIVE_TYPE);
         SmartDashboard.putBoolean("Squared Inputs", DRIVE_SQUAREDINPUTS);
         SmartDashboard.putBoolean("Brake", DRIVE_BRAKE);
+        
+        Heartbeat.add(skipped -> {
+            if(!(((String) driveChooser.getSelected()).equalsIgnoreCase(DRIVE_TYPE))) {
+                DRIVE_TYPE = (String) driveChooser.getSelected();
+                SmartDashboard.putString("Drive Type", DRIVE_TYPE);
+            }
+        });
+        
+        leftTalon = Registrar.canTalon(TALONS_LEFT[0]);
+        leftTalonF = Registrar.canTalon(TALONS_LEFT[1]);
+        rightTalon = Registrar.canTalon(TALONS_RIGHT[0]);
+        rightTalonF = Registrar.canTalon(TALONS_RIGHT[1]);
         
         leftTalon.changeControlMode(TalonControlMode.PercentVbus);
         rightTalon.changeControlMode(TalonControlMode.PercentVbus);
         leftTalonF.changeControlMode(TalonControlMode.Follower);
         rightTalonF.changeControlMode(TalonControlMode.Follower);
         
+        
         leftTalonF.set(leftTalon.getDeviceID());
         rightTalonF.set(rightTalon.getDeviceID());
+        
+        leftTalon.setInverted(TALONS_REVERSE_LEFT);
+        rightTalon.setInverted(TALONS_REVERSE_RIGHT);
+        
                         
         leftTalon.enableBrakeMode(DRIVE_BRAKE);
         leftTalonF.enableBrakeMode(DRIVE_BRAKE);
@@ -76,10 +117,12 @@ public class RobotModule extends IterativeModule {
     }
     
     public void teleopPeriodic() {
-            if(DRIVE_TYPE.toLowerCase().equals("tank")) {
+            if(DRIVE_TYPE.equalsIgnoreCase(driveTank)) {
                 robotDrive.tankDrive(driveJoystick.leftStick.getY(), driveJoystick.rightStick.getY(), DRIVE_SQUAREDINPUTS);
-            } else if (DRIVE_TYPE.toLowerCase().equals("arcade")) {
+            } else if (DRIVE_TYPE.equalsIgnoreCase(driveArcade)) {
                 robotDrive.arcadeDrive(driveJoystick.leftStick.getY(), driveJoystick.leftStick.getX(), DRIVE_SQUAREDINPUTS);
+            } else if (DRIVE_TYPE.equalsIgnoreCase(driveArcade2Stick)) {
+                robotDrive.arcadeDrive(driveJoystick.leftStick.getY(), driveJoystick.rightStick.getX(), DRIVE_SQUAREDINPUTS);
             }
     }
 }
